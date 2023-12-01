484abfd0fbff59b20fb23a0ced91d1778628a533
Sara Mirian <143549219+Sara-Mirian-Santos@users.noreply.github.com>
2023-12-01 15:33:31 -0300

    Update Home.dart
    Navegação das telas prontas

--- smart_house/smart_house/lib/Home.dart
+++ smart_house/smart_house/lib/Home.dart
@@ -1,6 +1,11 @@
 ///File download from FlutterViz- Drag and drop a tools. For more details visit https://flutterviz.io/
 
 import 'package:flutter/material.dart';
+import 'package:smart_house/quarto.dart';
+import 'package:smart_house/sala.dart';
+
+import 'banheiro.dart';
+import 'cozinha.dart';
 
 class home extends StatefulWidget {
   const home({super.key});
@@ -39,184 +44,128 @@ class _homeState extends State<home> {
             Padding(
               padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
               child:
-                  Container(
-                    margin: EdgeInsets.all(0),
-                    padding: EdgeInsets.all(0),
-                    width: 300,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: Color(0x00ffffff),
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.zero,
-                    ),
-                    child: Row(
+                    Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       mainAxisSize: MainAxisSize.max,
                       children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 20),
-                          child:
                             Container(
-                              width: 100,
-                              height: 100,
+                              width: 120,
+                              height: 120,
                               decoration: new BoxDecoration(
                                 color: Color.fromRGBO(83, 138, 245, 100),
                                 shape: BoxShape.circle,
                               ),
                               child: Padding(padding: EdgeInsets.all(10),
-                              child:ImageIcon(
-                                      AssetImage("images/quarto.png"),
-                                      size: 90,
-                                      color: Color(0xffffffff),
-                                    ),
+                                child: IconButton(
+                                  icon: ImageIcon(AssetImage("images/quarto.png")),
+                                  iconSize: 70,
+                                  color: Colors.white,
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => quarto()));
+                                  },
                                   ),
-                                ),
-
+                              ),
                         ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 20),
-                          child:Container(
-                            width: 100,
-                            height: 100,
+                          Container(
+                            width: 120,
+                            height: 120,
                             decoration: new BoxDecoration(
                               color: Color.fromRGBO(83, 138, 245, 100),
                               shape: BoxShape.circle,
                             ),
                             child: Padding(padding: EdgeInsets.all(10),
-                            child: ImageIcon(
-                            AssetImage("images/sala.png"),
-                            size: 90,
-                            color: Color(0xffffffff),
-                          ),
-                          ),
-                        ),
+                              child: IconButton(
+                                icon: ImageIcon(AssetImage("images/sala.png")),
+                                iconSize: 70,
+                                color: Colors.white,
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => sala()));
+                                },
+                              ),
+                            ),
                         ),
                       ],
                     ),
                   ),
-            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisSize: MainAxisSize.max,
               children: [
                 Container(
-                  margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                  padding: EdgeInsets.all(0),
-                  width: 300,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Color(0x00ffffff),
-                    shape: BoxShape.rectangle,
-                    borderRadius: BorderRadius.zero,
+                  width: 120,
+                  height: 120,
+                  decoration: new BoxDecoration(
+                    color: Color.fromRGBO(83, 138, 245, 100),
+                    shape: BoxShape.circle,
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                        child:Container(
-                        width: 100,
-                        height: 100,
-                        decoration: new BoxDecoration(
-                          color: Color.fromRGBO(83, 138, 245, 100),
-                          shape: BoxShape.circle,
-                        ),
-                          child: Padding(padding: EdgeInsets.all(10),
-                          child:ImageIcon(
-                          AssetImage("images/cozinha.png"),
-                          size: 90,
-                          color: Color(0xffffffff),
-                          ),
-                          ),
-                      ),
-                      ),
-                      Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                        child:Container(
-                        width: 100,
-                        height: 100,
-                        decoration: new BoxDecoration(
-                        color: Color.fromRGBO(83, 138, 245, 100),
-                        shape: BoxShape.circle,
-                        ),
-                        child: Padding(padding: EdgeInsets.all(10),
-                        child:
-                          ImageIcon(
-                          AssetImage("images/banheiro.png"),
-                          size: 90,
-                          color: Color(0xffffffff),
-                        ),
-                        ),
-                        )
-                      ),
-                    ],
+                  child: Padding(padding: EdgeInsets.all(10),
+                    child: IconButton(
+                      icon: ImageIcon(AssetImage("images/cozinha.png")),
+                      iconSize: 70,
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => cozinha()));
+                      },
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 120,
+                  height: 120,
+                  decoration: new BoxDecoration(
+                    color: Color.fromRGBO(83, 138, 245, 100),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Padding(padding: EdgeInsets.all(10),
+                    child: IconButton(
+                      icon: ImageIcon(AssetImage("images/banheiro.png")),
+                      iconSize: 70,
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => banheiro()));
+                      },
+                    ),
                   ),
                 ),
               ],
             ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Container(
-                    margin: EdgeInsets.all(0),
-                    padding: EdgeInsets.all(0),
-                    width: 300,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      color: Color(0x00ffffff),
-                      shape: BoxShape.rectangle,
-                      borderRadius: BorderRadius.zero,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 90),
-                        child:Container(
-                          width: 100,
-                          height: 100,
-                          decoration: new BoxDecoration(
-                            color: Color.fromRGBO(83, 138, 245, 100),
-                            shape: BoxShape.circle,
-                          ),
-                          child: Padding(padding: EdgeInsets.all(10),
-                            child: ImageIcon(
-                            AssetImage("images/mais.png"),
-                            size: 50,
-                            color: Color(0xffffffff),
-                          ),
-                          ),
-                            
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
             Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisSize: MainAxisSize.max,
-              children: [],
+              children: [
+              Container(
+              width: 80,
+              height: 80,
+              decoration: new BoxDecoration(
+                color: Color.fromRGBO(83, 138, 245, 100),
+                shape: BoxShape.circle,
+              ),
+              child: Padding(padding: EdgeInsets.all(10),
+                child: IconButton(
+                  icon: ImageIcon(AssetImage("images/mais.png")),
+                  iconSize: 50,
+                  color: Colors.white,
+                  onPressed: () {},
+                ),
+              ),
             ),
-
+              ],
+            ),
           ],
         ),
       ),
