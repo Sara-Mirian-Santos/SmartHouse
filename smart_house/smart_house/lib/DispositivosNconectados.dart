bdb757b1d2346bed900037f634cc1d70fac59b59
Jhonatas <94148715+Jhones0@users.noreply.github.com>
2023-11-30 22:02:45 -0300

    DispositivosNconectados.dart

--- smart_house/smart_house/lib/DispositivosNconectados.dart
+++ smart_house/smart_house/lib/DispositivosNconectados.dart
@@ -1,15 +1,90 @@
-import 'package:flutter/material.dart';
+Widget build(BuildContext context) {
+    return Scaffold(appBar:
+    AppBar(
+      centerTitle: true,
+      backgroundColor: Color.fromRGBO(197, 200, 196, 100),
+      // actions: [
+      //   IconButton(onPressed: () {
+      //     Navigator.push(
+      //       context,
+      //     MaterialPageRoute(
+      //         builder: (context) => Perfil()));
+      //   }, icon: Icon(Icons.account_circle_sharp))
+      // ],
+     ),
+    body:
+     Container(
+       width: MediaQuery.of(context).size.width,
+       height: MediaQuery.of(context).size.height,
+       color:  Color.fromRGBO(219, 219, 219, 100),
 
-class dispNconectados extends StatefulWidget {
-  const dispNconectados({super.key});
 
-  @override
-  State<dispNconectados> createState() => _dispNconectadosState();
-}
+       child:
+      Column(
+        mainAxisAlignment:MainAxisAlignment.start,
+        crossAxisAlignment:CrossAxisAlignment.center,
+        mainAxisSize:MainAxisSize.max,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0,50,0,0),
+            child: GestureDetector(
+              //onTap: _conectarCloud,
+              child: Image(
+                alignment: Alignment.center,
+                image: AssetImage("images/CloudON.png"),
+                height: 100,
+                width:140,
+                fit:BoxFit.cover,
+              ),
+            ),
+          ),
+          Text(
+            "Dispositivos conectados: qtdDisp",
+            textAlign: TextAlign.center,
+            style:TextStyle(
+            overflow:TextOverflow.clip,
+              fontWeight:FontWeight.w700,
+              fontStyle:FontStyle.normal,
+              fontSize:14,
+              color:Color(0xff2a1ed4),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0,100,0,0),
+            child: Container(
+              margin: EdgeInsets.all(0),
+              padding: EdgeInsets.all(0),
+              height: 100,
+              //color: Color.fromRGBO(196, 200, 197, 100),
+              child: Padding(padding: EdgeInsets.all(20),
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Color(0xff14e9a4),
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                    side: BorderSide(color: Color(0xff000000), width: 1),
+                  ),
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    "Mostrar todos os dispositivos",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal,
+                    ),
+                  ),
+                  textColor: Color(0xff000000),
+                  height: 40,
+                  minWidth: 140,
+                ),
+              ),
+            ),
+          ),
+        ],
 
-class _dispNconectadosState extends State<dispNconectados> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+
+    ),
+     )
+    );
   }
-}
